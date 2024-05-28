51af7891b9293e7e347b9742ef4bada47ff12daf
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-28 16:46:42 +0000

    add findByName func

--- src/main/java/com/machabooks/bookstore/repositories/RolRepository.java
+++ src/main/java/com/machabooks/bookstore/repositories/RolRepository.java
@@ -1,9 +1,12 @@
 package com.machabooks.bookstore.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.machabooks.bookstore.models.Rol;
 
 public interface RolRepository extends CrudRepository<Rol,Long>{
 
+	Optional<Rol> findByName(String name);
 }
