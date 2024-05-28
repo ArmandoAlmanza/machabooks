a1ce44801a48fab4879b5f878fcc37ca35986542
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-28 16:45:49 +0000

    erase unused imports and add post mapping

--- src/main/java/com/machabooks/bookstore/controllers/UserController.java
+++ src/main/java/com/machabooks/bookstore/controllers/UserController.java
@@ -9,8 +9,11 @@ import com.machabooks.bookstore.services.UserService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,6 +25,11 @@ public class UserController {
 	@GetMapping()
 	public List<User> userList() {
 		return service.findAll();
+	}
+
+	@PostMapping()
+	public ResponseEntity<?> create(@RequestBody User user) {
+		return service.create(user);
 	}
 
 }
