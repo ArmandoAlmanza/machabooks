c129888111561c26a6b2525d88920e9cc9ac1688
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-30 15:38:00 +0000

    add the book connection for authors

--- src/main/java/com/machabooks/bookstore/models/User.java
+++ src/main/java/com/machabooks/bookstore/models/User.java
@@ -2,15 +2,19 @@ package com.machabooks.bookstore.models;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
@@ -67,6 +71,10 @@ public class User {
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
 			@UniqueConstraint(columnNames = { "user_id", "role_id" }) })
 	private List<Rol> roles;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "author", cascade = CascadeType.ALL)
+	@JsonManagedReference
+	private List<Books> bookList;
 
 	@PrePersist
 	public void prePersist() {
