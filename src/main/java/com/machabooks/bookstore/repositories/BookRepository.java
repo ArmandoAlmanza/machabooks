1bee3d5f8741b3892e977604ba963cd17ed13737
Armando Almanza <armando.s.almanza@gmail.com>
2024-05-31 16:19:19 +0000

    add findBySKU and findByAuthor Method

--- src/main/java/com/machabooks/bookstore/repositories/BookRepository.java
+++ src/main/java/com/machabooks/bookstore/repositories/BookRepository.java
@@ -1,8 +1,14 @@
 package com.machabooks.bookstore.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.machabooks.bookstore.models.Books;
+import com.machabooks.bookstore.models.User;
 
 public interface BookRepository extends CrudRepository<Books, String> {
+	public Optional<Books> findBySku(String sku);
+
+	public Optional<Books> findByAuthor(User author);
 }
