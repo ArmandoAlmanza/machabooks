184561134eb9c9583328984b002bd0f0969a5fa0
Armando Almanza <armando.s.almanza@gmail.com>
2024-06-03 15:16:27 +0000

    erase the transient fields (unnused) and add loans

--- src/main/java/com/machabooks/bookstore/models/Books.java
+++ src/main/java/com/machabooks/bookstore/models/Books.java
@@ -4,18 +4,20 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -55,11 +57,9 @@ public class Books {
 	@JsonBackReference
 	private User author;
 
-	@Transient
-	private String authorMail;
-
-	@Transient
-	private String genre;
+	@OneToMany(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JsonManagedReference
+	private List<Loan> loans;
 
 	@PrePersist
 	public void prePersist() {
