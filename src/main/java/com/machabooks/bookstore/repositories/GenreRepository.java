f4efb109c62eeb4e71ab2b5bc962ea7121a0a745
Armando Almanza <armando.s.almanza@gmail.com>
2024-06-05 15:15:59 +0000

    change the method findByName

--- src/main/java/com/machabooks/bookstore/repositories/GenreRepository.java
+++ src/main/java/com/machabooks/bookstore/repositories/GenreRepository.java
@@ -1,6 +1,5 @@
 package com.machabooks.bookstore.repositories;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -9,5 +8,5 @@ import com.machabooks.bookstore.models.Genre;
 
 public interface GenreRepository extends CrudRepository<Genre, Long> {
 
-	Optional<Genre> findByName(List<Genre> list);
+	Optional<Genre> findByName(String name);
 }
