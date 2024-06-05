94893c6d5b71fe82eb888245f3f63658a005a8b2
Armando Almanza <armando.s.almanza@gmail.com>
2024-06-05 15:15:26 +0000

    add create method

--- src/main/java/com/machabooks/bookstore/controllers/LoanController.java
+++ src/main/java/com/machabooks/bookstore/controllers/LoanController.java
@@ -3,22 +3,31 @@ package com.machabooks.bookstore.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.machabooks.bookstore.models.Loan;
-import com.machabooks.bookstore.repositories.LoanRepository;
+import com.machabooks.bookstore.services.LoanService;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api/loans")
 public class LoanController {
 	@Autowired
-	private LoanRepository repository;
+	private LoanService service;
 
 	@GetMapping()
 	public List<Loan> findAll() {
-		return (List<Loan>) repository.findAll();
+		return service.findAll();
+	}
+
+	@PostMapping("/borrow/{email}/{sku}")
+	public ResponseEntity<?> create(@PathVariable String email, @PathVariable String sku) {
+		return service.create(email, sku);
 	}
 
 }
