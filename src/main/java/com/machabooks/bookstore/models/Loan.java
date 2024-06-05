bb7836389012af7142af705bcb11947d41dd2b20
Armando Almanza <armando.s.almanza@gmail.com>
2024-06-05 15:15:37 +0000

    add the generated type

--- src/main/java/com/machabooks/bookstore/models/Loan.java
+++ src/main/java/com/machabooks/bookstore/models/Loan.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -24,6 +26,7 @@ import lombok.Setter;
 @Table(name = "loans")
 public class Loan {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "start_date")
