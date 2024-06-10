4f2731ebb0665e79f142a87604ca73aac8c482ba
armando.s.almanza <armando.s.almanza@gmail.com>
2024-06-10 17:42:33 +0000

    add how i want to look

--- Client/src/pages/LandingPage.tsx
+++ Client/src/pages/LandingPage.tsx
@@ -1,22 +1,42 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const links = [{path: "/", name: "Home", active: false}, {path: "/about", name: "About", active: false}];
+  const links = [
+    { path: "/", name: "Home", active: false },
+    { path: "/about", name: "About", active: false },
+  ];
   return (
     <>
       <header className="flex flex-row justify-evenly items-end px-4 py-8 content-center">
-        <h1 className="text-violet-400 text-4xl font-bold cursor-pointer">MachaBooks</h1>
+        <h1 className="text-violet-400 text-4xl font-bold cursor-pointer">
+          MachaBooks
+        </h1>
         <nav className="flex flex-row gap-4">
           {links.map((link, i) => (
-            <Link key={i} className="hover:text-violet-400 transition-colors duration-300" to={link.path}>
+            <Link
+              key={i}
+              className="hover:text-violet-400 transition-colors duration-300"
+              to={link.path}
+            >
               {link.name}
             </Link>
           ))}
         </nav>
       </header>
-      <main>
-        <p>This is the main content</p>
-        <p>Well this is my first poage... what know?</p>
+      <main className="container mx-auto max-w-2xl grid place-items-center">
+        <div className="w-full flex gap-4 justify-between items-center">
+          <div className="grid gap-2">
+            <h1 className="text-5xl font-bold">
+              Welcome to <span className="text-purple-400">MachaBooks</span>{" "}
+            </h1>
+            <p>Well this is my first poage... what know?</p>
+          </div>
+          <img
+            className="w-1/2"
+            src="https://th.bing.com/th/id/OIP.xpw9XeB4YEpgD44xgLzkcAHaHa?rs=1&pid=ImgDetMain"
+            alt=""
+          />
+        </div>
       </main>
     </>
   );
