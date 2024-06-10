f7f3a2c385a2a3da6fe5ce9d3b7260fb5442e85b
armando.s.almanza <armando.s.almanza@gmail.com>
2024-06-10 17:15:39 +0000

    add clsx

--- Client/src/pages/LandingPage.tsx
+++ Client/src/pages/LandingPage.tsx
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
+  const links = [{path: "/", name: "Home", active: false}, {path: "/about", name: "About", active: false}];
   return (
     <>
-      <header className="flex flex-row justify-evenly items-center px-4 py-8">
-        <h1>Hello World</h1>
+      <header className="flex flex-row justify-evenly items-end px-4 py-8 content-center">
+        <h1 className="text-violet-400 text-4xl font-bold cursor-pointer">MachaBooks</h1>
         <nav className="flex flex-row gap-4">
-          <a>Home</a>
-          <a>About</a>
-          <a>Contact</a>
+          {links.map((link, i) => (
+            <Link key={i} className="hover:text-violet-400 transition-colors duration-300" to={link.path}>
+              {link.name}
+            </Link>
+          ))}
         </nav>
       </header>
       <main>
         <p>This is the main content</p>
+        <p>Well this is my first poage... what know?</p>
       </main>
     </>
   );
