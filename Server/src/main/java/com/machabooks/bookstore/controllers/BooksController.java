0d65a417af49ffe13ed382317d02f158b1b3d706
Armando Almanza <armando.s.almanza@gmail.com>
2024-06-20 17:02:26 +0000

    add put

--- Server/src/main/java/com/machabooks/bookstore/controllers/BooksController.java
+++ Server/src/main/java/com/machabooks/bookstore/controllers/BooksController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -43,7 +44,7 @@ public class BooksController {
 		return service.create(book, file);
 	}
 
-	@PostMapping("/{sku}/cover")
+	@PutMapping("/{sku}/cover")
 	public ResponseEntity<?> addCover(@PathVariable String sku, @PathVariable MultipartFile file) {
 		return service.addCover(sku, file);
 	}
